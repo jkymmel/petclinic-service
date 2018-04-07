98b52e02c19acb80bc9516b64073930559baf047
Johannes Kümmel <jkymmel@gmail.com>
2018-04-07 23:37:19 +0300

    Create repos for Veterinarian and Visit

---
+++ server/src/main/java/io/github/jkymmel/idu0075/petclinic/server/veterinarian/VeterinarianRepository.java
@@ -0,0 +1,6 @@
+package io.github.jkymmel.idu0075.petclinic.server.veterinarian;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VeterinarianRepository extends CrudRepository<VeterinarianRepository, String> {
+}
---
+++ server/src/main/java/io/github/jkymmel/idu0075/petclinic/server/visit/VisitRepository.java
@@ -0,0 +1,6 @@
+package io.github.jkymmel.idu0075.petclinic.server.visit;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VisitRepository extends CrudRepository<Visit, String> {
+}

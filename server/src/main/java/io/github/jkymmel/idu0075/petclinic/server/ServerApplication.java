5338cdae8ed77161f41a9a24512a08bf53438b16
Johannes Kümmel <jkymmel@gmail.com>
2018-04-07 23:12:10 +0300

    init

---
+++ server/src/main/java/io/github/jkymmel/idu0075/petclinic/server/ServerApplication.java
@@ -0,0 +1,12 @@
+package io.github.jkymmel.idu0075.petclinic.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}

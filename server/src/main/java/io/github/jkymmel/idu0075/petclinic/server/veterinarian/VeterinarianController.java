7ed7f952adc9e0d258d20f7f470fa59b9ce781ff
Johannes Kümmel <jkymmel@gmail.com>
2018-04-07 23:55:58 +0300

    Create controllers for Veterinarian and Visit

---
+++ server/src/main/java/io/github/jkymmel/idu0075/petclinic/server/veterinarian/VeterinarianController.java
@@ -0,0 +1,33 @@
+package io.github.jkymmel.idu0075.petclinic.server.veterinarian;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Optional;
+
+@RestController(value = "/veterinarian")
+public class VeterinarianController {
+
+    @Resource
+    private VeterinarianService veterinarianService;
+
+    @GetMapping
+    private ResponseEntity<List<Veterinarian>> findAll() {
+        return new ResponseEntity<>(veterinarianService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/:uuid")
+    private ResponseEntity<Veterinarian> findByUuid(@PathVariable String uuid) {
+        return veterinarianService.findByUuid(uuid)
+                .map(veterinarian -> new ResponseEntity<>(veterinarian, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    private ResponseEntity<Veterinarian> save(@RequestBody Veterinarian veterinarian) {
+        return new ResponseEntity<>(veterinarianService.save(veterinarian), HttpStatus.OK);
+    }
+}

3f2154ca84a8966558deafb4965f30adf8975975
Johannes Kümmel <jkymmel@gmail.com>
2018-06-04 09:42:32 +0300

    Added client

---
+++ client/client.py
@@ -0,0 +1,51 @@
+import datetime
+
+from pysimplesoap.client import SoapClient
+
+print('Starting up!')
+
+client = SoapClient(wsdl='http://localhost:8080/ws/PetClinicWebService.wsdl', trace=False)
+
+api_key = input('Please enter API key: ')
+
+def format_pet(pet):
+    return f'PET:\n  + ID: {pet["id"]}\n  + name: {pet["name"]}\n  + owner: {pet["owner"]}\n' \
+           f'  + veterinarian: {format_vet(pet.get("veterinarian", None))}'
+
+def format_vet(vet):
+    if vet != None:
+        return f'\n    + ID: {vet["id"]}\n    + name: {vet["name"]}\n    + email: {vet["email"]}\n' \
+           f'    + phone number: {vet["phoneNumber"]}\n    + personal code: {vet["personalCode"]}'
+    else:
+        return 'None'
+    
+
+while(True):
+    operation = input(
+        'Available operations:\n0: List all pets\n1: Add new Pet\n9: Exit\nPlease enter the number of operation: ')
+    if operation == '0':
+        pets = client.GetAllPets(
+            apiKey=api_key
+        )
+        print('\nPETS:\n')
+        for pet in pets['pet']:
+            print(format_pet(pet))
+        print('\n\n')
+    elif operation == '1':
+        name = input('Enter name: ')
+        owner = input('Enter owner: ')
+        birthday = input('Enter birthday (YYYY-MM-dd): ')
+        pet = client.SavePet(
+            pet={
+                'name': name,
+                'owner': owner,
+                'birthday': datetime.date
+            }
+        )
+        print('Saved!')
+        print(pet['pet'])
+        print('\n\n')
+    elif operation == '9':
+        exit(0)
+    else:
+        print('Unknown operation!\n\n')
